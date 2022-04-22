88a40b4808df998b8735d95d18e884142de020d0
jacg <jacg311@github.fake>
2022-04-22 20:33:47 +0200

    dunno where that file came from.

--- src/main/java/net/jacg/better_beaches/mixin/MClientMixin.java
+++
@@ -1,9 +0,0 @@
-package net.jacg.better_beaches.mixin;
-
-import net.minecraft.client.MinecraftClient;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(MinecraftClient.class)
-public class MClientMixin {
-
-}
